63274f109bf1781432d771cb1a7683865a565bd7
Louis <louisr.daviaud@gmail.com>
2016-09-20 00:56:50 +0200

    Update GrapheMatrice.java

--- math/tp1/GrapheMatrice.java
+++ math/tp1/GrapheMatrice.java
@@ -1,4 +1,4 @@
-package math;
+package math.tp1;
 
 import java.util.Scanner;
 
@@ -10,7 +10,18 @@ public final class GrapheMatrice
 	
 	public GrapheMatrice()
 	{
-		aretes = new int [0][0];
+		nombre_sommets  = 0;
+		aretes 			= new int [0][0];
+		composantes 	= new int [0];
+	}
+	
+	public GrapheMatrice(int p_nombre_sommets, int p_nombre_aretes) throws Exception
+	{
+		if((p_nombre_sommets * (p_nombre_sommets - 1) / 2 < p_nombre_aretes))
+				throw new Exception("Nombre d'aretes trop eleve!");
+		nombre_sommets = p_nombre_sommets;
+		aretes = new int [p_nombre_aretes][2];
+		composantes = new int [nombre_sommets];	
 	}
 	
 	public GrapheMatrice(int p_nombre_sommets, int ... p_aretes)
@@ -25,110 +36,146 @@ public final class GrapheMatrice
 		composantes = new int [nombre_sommets];	
 	}
 	
-	public GrapheMatrice(int p_nombre_sommets, int p_nombre_aretes) throws Exception
+	private final void genererAretesRandom()
 	{
-		if((p_nombre_sommets * (p_nombre_sommets - 1) / 2 < p_nombre_aretes))
-				throw new Exception("Nombre d'aretes trop eleve!");
-		nombre_sommets = p_nombre_sommets;
-		aretes = new int [p_nombre_aretes][2];
-		composantes = new int [nombre_sommets];	
+		for(int ligne = 0; ligne < aretes.length; ligne++)
+		{
+			int t_x_random = valeurRandom(0, nombre_sommets);
+			int t_y_random = valeurRandom(0, nombre_sommets);
+			if(!dejaPris(t_x_random, t_y_random) && t_x_random != t_y_random)
+			{
+				aretes[ligne][0] = t_x_random;
+				aretes[ligne][1] = t_y_random;
+			}
+			else
+			{
+				while(dejaPris(t_x_random, t_y_random) || t_x_random == t_y_random)
+				{
+					t_x_random = valeurRandom(0, nombre_sommets);
+					t_y_random = valeurRandom(0, nombre_sommets);
+				}
+				aretes[ligne][0] = t_x_random;
+				aretes[ligne][1] = t_y_random;
+			}
+		}
+	}
+		
+	private final void genererComposantes()
+	{
+		for(int indice = 0; indice < nombre_sommets; indice++)
+			composantes[indice] = indice;
+		
+		for(int ligne = 0; ligne < aretes.length; ligne++)
+		{
+			int x = aretes[ligne][0];
+			int y = aretes[ligne][1];
+			if(composantes[x] != composantes[y])
+			{
+				int aux = composantes[x];
+				for(int z = 0; z < nombre_sommets; z++)
+				{
+					if(composantes[z] == aux)
+						composantes[z] = composantes[y];
+				}
+			}
+				
+		}
 	}
 	
-		private void genererAretesRandom()
+	public final void ecrituretailles()
+	{
+		int [] compteur_composantes = new int [nombre_sommets];
+		for(int index = 0; index < composantes.length; index++)
+			compteur_composantes[composantes[index]]++;
+		ecrituretailles_aux_points_isoles(compteur_composantes);
+		ecrituretailles_aux_composantes_taille(compteur_composantes);
+	}
+	
+	public final void ecrituretailles_aux_points_isoles(int [] p_compteur_composantes)
+	{
+		int compteur_composantes_isoles = 0;
+		for(int index = 0; index < p_compteur_composantes.length; index++)
+			if(p_compteur_composantes[index] == 1)
+				compteur_composantes_isoles++;
+		System.out.println("Il y a " + compteur_composantes_isoles + " points isoles");
+	} 
+	
+	public final void ecrituretailles_aux_composantes_taille(int [] p_compteur_composantes)
+	{
+		int compteur;
+		int taille_courante = 0;
+		for(int i = 0; i < p_compteur_composantes.length; i++)
 		{
-			for(int ligne = 0; ligne < aretes.length; ligne++)
+			compteur = 0;
+			taille_courante = p_compteur_composantes[i];
+			j : for(int j = 0; j < composantes.length; j++)
 			{
-				int t_x_random = valeurRandom(0, nombre_sommets);
-				int t_y_random = valeurRandom(0, nombre_sommets);
-				if(!dejaPris(t_x_random, t_y_random) && t_x_random != t_y_random)
+				if(taille_courante == 0)
+					break j;
+				else if(p_compteur_composantes[j] == taille_courante) 
 				{
-					aretes[ligne][0] = t_x_random;
-					aretes[ligne][1] = t_y_random;
-				}
-				else
-				{
-					while(dejaPris(t_x_random, t_y_random) || t_x_random == t_y_random)
-					{
-						t_x_random = valeurRandom(0, nombre_sommets);
-						t_y_random = valeurRandom(0, nombre_sommets);
-					}
-					aretes[ligne][0] = t_x_random;
-					aretes[ligne][1] = t_y_random;
+					compteur++;
 				}
 			}
+			if(taille_courante > 1)
+				System.out.println("Il y a " + compteur + " composantes de taille " + taille_courante);
+			else if(taille_courante == 1)
+				System.out.println("Il y a " + compteur + " composante de taille " + taille_courante);
 		}
-		
-		private void genererComposantes()
-		{
-			for(int indice = 0; indice < nombre_sommets; indice++)
-				composantes[indice] = indice;
-			
-			for(int ligne = 0; ligne < aretes.length; ligne++)
-			{
-				int x = aretes[ligne][0];
-				int y = aretes[ligne][1];
-				if(composantes[x] != composantes[y])
-				{
-					int aux = composantes[x];
-					for(int z = 0; z < nombre_sommets; z++)
-					{
-						if(composantes[z] == aux)
-							composantes[z] = composantes[y];
-					}
-				}
-					
-			}
-		}
-		
-		private boolean dejaPris(int p_x, int p_y)
-		{
-			for(int ligne = 0; ligne < aretes.length; ligne++)
-				if((aretes[ligne][0] == p_x && aretes[ligne][1] == p_y) || 
-						(aretes[ligne][0] == p_y && aretes[ligne][1] == p_x))
-					return true;
-			return false;
-		}
-		
-		private static int saisireInt(String p_message)
-		{
-			System.out.print(p_message);
-			return new Scanner(System.in).nextInt();
-		}
-		
-		private static int valeurRandom(int p_min, int p_max)
-		{
-			return (int) (Math.random() * (p_max - p_min) + p_min);
-		}
-		
-		public String toString()
-		{
-			return "";
-		}
-		
-		private void print_aretes()
-		{
-			for(int ligne = 0; ligne < aretes.length; ligne++)
-				System.out.println("{" + aretes[ligne][0] + "," + aretes[ligne][1] + "}");
-		}
-		
-		private void print_composantes()
-		{
-			for(int indice = 0; indice < composantes.length; indice++)
-				System.out.print(composantes[indice] + ", ");
-		}
+	}
+	
+	private boolean dejaPris(int p_x, int p_y)
+	{
+		for(int ligne = 0; ligne < aretes.length; ligne++)
+			if((aretes[ligne][0] == p_x && aretes[ligne][1] == p_y) || 
+					(aretes[ligne][0] == p_y && aretes[ligne][1] == p_x))
+				return true;
+		return false;
+	}
+	
+	private static int saisireInt(String p_message)
+	{
+		System.out.print(p_message);
+		return new Scanner(System.in).nextInt();
+	}
+	
+	private static int valeurRandom(int p_min, int p_max)
+	{
+		return (int) (Math.random() * (p_max - p_min) + p_min);
+	}
+	
+	public String toString()
+	{
+		return "";
+	}
+	
+	private final void print_aretes()
+	{
+		for(int ligne = 0; ligne < aretes.length; ligne++)
+			System.out.println("{" + aretes[ligne][0] + "," + aretes[ligne][1] + "}");
+	}
+	
+	private final void print_composantes()
+	{
+		for(int indice = 0; indice < composantes.length; indice++)
+			System.out.print(composantes[indice] + ", ");
+		System.out.println("");
+	}
 
-		public static void main(String [] Args) throws Exception
-		{
-			GrapheMatrice graphe_matrice = new GrapheMatrice(3, 1);
-			
-			/** Debut : Excercice 1 **/
-			
-			graphe_matrice.genererAretesRandom();
-			graphe_matrice.genererComposantes();
-			graphe_matrice.print_aretes();
-			System.out.println("");
-			graphe_matrice.print_composantes();
-			
-			/** Fin : Excercice 1 **/
-		}
+	public static void main(String [] Args) throws Exception
+	{
+		GrapheMatrice graphe_matrice = new GrapheMatrice(10,4);
+		
+		/** Debut : Excercice 1 **/
+		
+		graphe_matrice.genererAretesRandom();
+		graphe_matrice.genererComposantes();
+		graphe_matrice.print_aretes();
+		System.out.println("");
+		graphe_matrice.print_composantes();
+		System.out.println("");
+		graphe_matrice.ecrituretailles();
+		
+		/** Fin : Excercice 1 **/
+	}
 }
