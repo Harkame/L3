f191fb5aa251f80bc823a07db2d402a973dee15d
Louis <louisr.daviaud@gmail.com>
2017-01-23 00:21:32 +0100

    Update analex1.c

--- src/compilation/tp1/analex1.c
+++ src/compilation/tp1/analex1.c
@@ -1,20 +1,19 @@
-/**
- * @file analex.c  
- * @author Michel Meynard
- * @brief Prog principal appelant analex()
- */
 #include <stdio.h>
 #include <string.h>
-#include "afd.h"		/* DÃ©finition de l'AFD et des JETONS */
-#include "analex1.h"		/* DÃ©finition de la fon : int analex() */
+#include "afd.h"
+#include "analex1.h"
 
-int main(){			/* Construction de l'AFD */
-  int j;			/* jeton retournÃ© par analex() */
-  char *invite="Saisissez un(des) mot(s) matchant a(b+c)?|bd, (les bd sont considÃ©rÃ©s comme des commentaires donc sont filtrÃ©s) suivi de EOF (CTRL-D) SVP : ";
-  creerAfd();			/* Construction de l'AFD Ã  jeton */
-  printf("%s",invite);		/* prompt */
-  while((j=analex())){		/* analyser tq pas jeton 0 */
-    printf("\nRÃ©sultat : Jeton = %d ; Lexeme = %s\n%s",j,lexeme,invite);
+int main()
+{
+  int j;			/* jeton retourné par analex() */
+  char* invite = "Saisissez un(des) mot(s) matchant a(b+c)?|bd, (les bd sont considérés comme des commentaires donc sont filtrés) suivi de EOF (CTRL-D) SVP : ";
+  creerAfd();
+  printf("%s",invite);
+
+  while((j = analex()))
+  {
+    printf("\nRésultat : Jeton = %d ; Lexeme = %s\n%s",j,lexeme,invite);
   }
+
   return 0;
 }
